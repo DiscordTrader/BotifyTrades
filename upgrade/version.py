22bb35598a4e29757df184cf3ed1de8e2d9e07e5
DiscordTrader <uday329@gmail.com>
2026-01-10 02:02:25 +0000

    Release v4.1.2 (USER build)

--- upgrade/version.py
+++ upgrade/version.py
@@ -16,7 +16,7 @@ from typing import Optional, Tuple, Dict
 
 
 APP_VERSION = "4.1.2"
-BUILD_DATE = "2026-01-09"
+BUILD_DATE = "2026-01-10"
 
 
 def parse_version(version_str: str) -> Tuple[int, int, int]:
