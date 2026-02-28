f1a27f423dc473ca611ed7503333a0a21b811558
DiscordTrader <uday329@gmail.com>
2026-02-28 06:26:23 +0000

    Release v5.2.7 (USER build)

--- upgrade/version.py
+++ upgrade/version.py
@@ -15,8 +15,8 @@ from pathlib import Path
 from typing import Optional, Tuple, Dict
 
 
-APP_VERSION = "5.2.6"
-BUILD_DATE = "2026-02-27"
+APP_VERSION = "5.2.7"
+BUILD_DATE = "2026-02-28"
 
 
 def parse_version(version_str: str) -> Tuple[int, int, int]:
